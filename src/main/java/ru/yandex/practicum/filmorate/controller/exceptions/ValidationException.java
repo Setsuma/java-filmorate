6d28471b28989c2a08732bc24f7c7d683a183a5d
Setsuma <122569837+Setsuma@users.noreply.github.com>
2023-12-13 09:44:15 +0300

    Delete src/main/java/ru/yandex/practicum/filmorate/controller/exceptions/ValidationException.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.controller.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
