305f77a4be768720993aac109464dee88baed668
Setsuma <122569837+Setsuma@users.noreply.github.com>
2023-12-19 17:12:04 +0300

    Delete src directory

--- src/main/java/ru/yandex/practicum/filmorate/exception/IdNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class IdNotFoundException extends RuntimeException {
-    public IdNotFoundException(String message) {
-        super(message);
-    }
-}
